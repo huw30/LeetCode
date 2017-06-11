8e4c25e9f08f54f905aceab5fe81b30a4a0038ff
Raina Wang <raina@weebly.com>
2017-06-11 15:21:19 -0700

    Selection sort

---
+++ Sort/selection.js
@@ -0,0 +1,20 @@
+var selection = function(arr) {
+  var temp;
+  for (var i = 0; i < arr.length; i++) {
+    var minIndex = i;
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    temp = arr[i];
+    arr[i] = arr[minIndex];
+    arr[minIndex] = temp;
+  }
+
+  return arr;
+}
+
+var test = [4, 1, 3, 9, 7];
+console.log(selection(test));
+//[1, 3, 4, 7, 9]
