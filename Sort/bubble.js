d6ae0534152ff737ef0077f535ea04e8bb37654f
Raina Wang <raina@weebly.com>
2017-06-12 13:18:27 -0700

    Formatting

--- Sort/bubble.js
+++ Sort/bubble.js
@@ -1,20 +1,20 @@
 // Iteratively
 var bubbleSort = function(arr) {
 	if (arr.length === 0 || arr.length === 1) {
-	return arr;
-  }
+		return arr;
+	}
 
 	var temp;
 	for (var i = 0; i < arr.length; i++) {
-	for (var j = 0; j < arr.length - i - 1; j++) {
-		if (arr[j] > arr[j+1]) {
-		temp = arr[j];
-		arr[j] = arr[j+1];
-		arr[j+1] = temp;
-		} 	
+		for (var j = 0; j < arr.length - i - 1; j++) {
+			if (arr[j] > arr[j+1]) {
+				temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			} 
+		}
 	}
-  }
-  return arr;
+	return arr;
 };
 
 console.log(bubbleSort([5,4,3,2,1]));
--- Sort/selection.js
+++ Sort/selection.js
@@ -1,18 +1,18 @@
 var selection = function(arr) {
-  var temp;
-  for (var i = 0; i < arr.length; i++) {
-    var minIndex = i;
-    for (var j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[minIndex]) {
-        minIndex = j;
-      }
-    }
-    temp = arr[i];
-    arr[i] = arr[minIndex];
-    arr[minIndex] = temp;
-  }
+	var temp;
+	for (var i = 0; i < arr.length; i++) {
+		var minIndex = i;
+		for (var j = i + 1; j < arr.length; j++) {
+			if (arr[j] < arr[minIndex]) {
+				minIndex = j;
+			}
+		}
+		temp = arr[i];
+		arr[i] = arr[minIndex];
+		arr[minIndex] = temp;
+	}
 
-  return arr;
+	return arr;
 }
 
 var test = [4, 1, 3, 9, 7];
